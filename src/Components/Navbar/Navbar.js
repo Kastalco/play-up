7db2296d53f41f61391df4a98b6580d56535599f
Kastalco <kamilo.alomias@uao.edu.co>
2021-05-18 09:09:07 -0500

    gg

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Navbar.css';
+
+const Navbar= function(){
+    
+    return(
+
+        
+            <div className='nav'>
+
+            </div>
+        
+        
+
+    );
+}
+
+export default Navbar;
---
+++ src/features/counter/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyACMvK2AF7846WumxyioeGC-BKPn0XBHas",
+    authDomain: "play-upf.firebaseapp.com",
+    projectId: "play-upf",
+    storageBucket: "play-upf.appspot.com",
+    messagingSenderId: "730279851081",
+    appId: "1:730279851081:web:e060fe6375c9c0dfe67b98"
+  };
